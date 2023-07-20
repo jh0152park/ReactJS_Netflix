9f48cd401a59e82637da57414405dfde039b976c
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-07-20 16:51:36 +0900

    created header navigation bar

--- netflix/src/App.tsx
+++ netflix/src/App.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Tv from "./Routes/Tv";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import Header from "./Routes/Components/Header";
 
 function App() {
-    return <h1>Hello World</h1>;
+    return (
+        <Router>
+            <Header></Header>
+            <Switch>
+                <Route path="/">
+                    <Home></Home>
+                </Route>
+                <Route path="/tv">
+                    <Tv></Tv>
+                </Route>
+                <Route path="/search">
+                    <Search></Search>
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default React.memo(App);
