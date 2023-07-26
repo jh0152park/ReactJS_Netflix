3660b535b895fdc7b45ca5221745418f9aa946e3
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-07-26 17:15:53 +0900

    added protect poster image

--- netflix/src/api.ts
+++ netflix/src/api.ts
@@ -69,7 +69,7 @@ export function getUpcomingMovies() {
     };
 
     return fetch(
-        "https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1",
+        "https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=2",
         options
     ).then((response) => response.json());
 }
