1212884a6749287e7991ccb0744eec278402ce08
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-07-26 15:03:52 +0900

    chagned icons at main display bannder

--- netflix/src/Routes/Components/MainDisplay.tsx
+++ netflix/src/Routes/Components/MainDisplay.tsx
@@ -1,6 +1,4 @@
 import { makeImagePath } from "../../utils";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faInfoCircle, faPlay } from "@fortawesome/free-solid-svg-icons";
 import {
     Banner,
     Buttons,
@@ -24,7 +22,20 @@ function MainDisplay({ title, overview, bgIamgePath }: IDisplay) {
 
             <Buttons>
                 <PlayButton whileHover={{ cursor: "pointer", opacity: 0.8 }}>
-                    <FontAwesomeIcon icon={faPlay} size="xl" />
+                    <svg
+                        width="30"
+                        height="30"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        data-name="Play"
+                        aria-hidden="true"
+                    >
+                        <path
+                            d="M5 2.69127C5 1.93067 5.81547 1.44851 6.48192 1.81506L23.4069 11.1238C24.0977 11.5037 24.0977 12.4963 23.4069 12.8762L6.48192 22.1849C5.81546 22.5515 5 22.0693 5 21.3087V2.69127Z"
+                            fill="currentColor"
+                        ></path>
+                    </svg>
                     <span>Play</span>
                 </PlayButton>
                 <MoreInfoButton
@@ -33,7 +44,22 @@ function MainDisplay({ title, overview, bgIamgePath }: IDisplay) {
                         backgroundColor: "rgba(73, 74, 68, 0.6)",
                     }}
                 >
-                    <FontAwesomeIcon icon={faInfoCircle} size="xl" />
+                    <svg
+                        width="30"
+                        height="30"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        data-name="CircleI"
+                        aria-hidden="true"
+                    >
+                        <path
+                            fill-rule="evenodd"
+                            clip-rule="evenodd"
+                            d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM0 12C0 5.37258 5.37258 0 12 0C18.6274 0 24 5.37258 24 12C24 18.6274 18.6274 24 12 24C5.37258 24 0 18.6274 0 12ZM13 10V18H11V10H13ZM12 8.5C12.8284 8.5 13.5 7.82843 13.5 7C13.5 6.17157 12.8284 5.5 12 5.5C11.1716 5.5 10.5 6.17157 10.5 7C10.5 7.82843 11.1716 8.5 12 8.5Z"
+                            fill="currentColor"
+                        ></path>
+                    </svg>
                     <span>More Info</span>
                 </MoreInfoButton>
             </Buttons>
